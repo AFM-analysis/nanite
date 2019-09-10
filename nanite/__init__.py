d0b3d07122149830a65f3a87f281f68f9b8b3e5a
Paul Müller <dev@craban.de>
2019-09-10 14:27:16 +0200

    fix: add __version__ property

--- nanite/__init__.py
+++ nanite/__init__.py
@@ -2,3 +2,5 @@ from .group import IndentationGroup, load_group  # noqa: F401
 from .indent import Indentation  # noqa: F401
 from .qmap import QMap  # noqa: F401
 from .rate import IndentationRater  # noqa: F401
+
+from ._version import version as __version__
