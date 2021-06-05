90560221d347f67be83a18e0c7d798c384e0b9f4
Paul Müller <dev@craban.de>
2021-06-05 20:37:10 +0200

    ref: better warning traceback for deprecated weight_cp method

--- nanite/model/weight.py
+++ nanite/model/weight.py
@@ -1,8 +1,11 @@
 import warnings
 
-from .residuals import compute_contact_point_weights as weight_cp  # noqa: F401
+from .residuals import compute_contact_point_weights  # noqa: F401
 
-warnings.warn(
-    "The 'weight' module is deprecated. Please use "
-    "'from nanite.model.residuals import compute_contact_point_weights'!",
-    DeprecationWarning)
+
+def weight_cp(*args, **kwargs):
+    warnings.warn(
+        "The 'weight' module is deprecated. Please use "
+        "'from nanite.model.residuals import compute_contact_point_weights'!",
+        DeprecationWarning)
+    return compute_contact_point_weights(*args, **kwargs)
