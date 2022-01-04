68beb47b0739755b4c6840c956dd5fc84f9af8fd
Paul Müller <dev@craban.de>
2022-01-04 11:52:18 +0100

    tests: reorder

--- tests/test_model_core.py
+++ tests/test_model_core.py
@@ -11,6 +11,25 @@ from common import MockModelModule
 data_path = pathlib.Path(__file__).parent / "data"
 
 
+def test_compute_anc_max_indent():
+    ds1 = nanite.IndentationGroup(
+        data_path / "fmt-jpk-fd_spot3-0192.jpk-force")
+    fd = ds1[0]
+    # correct for an offset in the tip
+    fd.apply_preprocessing(preprocessing=["compute_tip_position",
+                                          "correct_tip_offset"],
+                           options={
+                               "correct_tip_offset": {
+                                   "method": "deviation_from_baseline"
+                               }
+                           })
+    # fit a model
+    fd.fit_model(model_key="hertz_para")
+    # get the ancillary parameter
+    max_indent = nanite.model.core.compute_anc_max_indent(fd)
+    assert np.allclose(max_indent, 1.2950734601921855e-07, atol=0, rtol=1e-8)
+
+
 def test_model_incomplete():
     class BadModel:
         model_key = "peterpan"
@@ -26,14 +45,6 @@ def test_model_incomplete_anc():
     with pytest.raises(nanite.model.core.ModelIncompleteError,
                        match="parameter_anc_keys"):
         nanite.model.core.NaniteFitModel(bad_mod)
-
-
-def test_model_repr_str():
-    with MockModelModule(model_key="peterpan") as md:
-        assert "peterpan" in repr(md)
-        assert "NaniteFitModel" in repr(md)
-        assert "peterpan" in str(md)
-        assert "NaniteFitModel" in str(md)
 
 
 def test_model_get_anc_parm_keys():
@@ -82,20 +93,9 @@ def test_model_get_parm_unit():
             md.get_parm_unit("peter")
 
 
-def test_compute_anc_max_indent():
-    ds1 = nanite.IndentationGroup(
-        data_path / "fmt-jpk-fd_spot3-0192.jpk-force")
-    fd = ds1[0]
-    # correct for an offset in the tip
-    fd.apply_preprocessing(preprocessing=["compute_tip_position",
-                                          "correct_tip_offset"],
-                           options={
-                               "correct_tip_offset": {
-                                   "method": "deviation_from_baseline"
-                               }
-                           })
-    # fit a model
-    fd.fit_model(model_key="hertz_para")
-    # get the ancillary parameter
-    max_indent = nanite.model.core.compute_anc_max_indent(fd)
-    assert np.allclose(max_indent, 1.2950734601921855e-07, atol=0, rtol=1e-8)
+def test_model_repr_str():
+    with MockModelModule(model_key="peterpan") as md:
+        assert "peterpan" in repr(md)
+        assert "NaniteFitModel" in repr(md)
+        assert "peterpan" in str(md)
+        assert "NaniteFitModel" in str(md)
